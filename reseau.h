61227fb8316d2e23d5a4db773e2779597183ab0c
MasterEwink <ewokisamail@gmail.com>
2017-01-24 17:23:56 +0100

    Update reseau.h

--- reseau.h
+++ reseau.h
@@ -1,36 +1,8 @@
 /*
     Neural Network
     reseau.h
-        CrÈÈ : 09/09/16
-        DÈlaration de la classe rÈseau
-    Historique des modifications :
-        10/09/16    - Ajout des directives de prÈprocesseur
-                    - Ajout de la classe Reseau
-        11/09/16    - Modification de m_liaisons en m_liaisonsE
-                    - Ajout de m_liaisonsF, Reseau (deux constructeurs), getEntree, getCouchesInternes, getSortie
-        15/09/16    - Suppression de m_couchesInternes, getCouchesInternes, m_liaisonsF
-                    - Modification de m_liaisonsE en m_liaisons
-        16/09/16    - Ajout de ~Reseau, getNbEntrees, getNbSorties, setEntrees, calculeSorties
-        28/09/16    - Modification de setEntrees
-        13/10/16    - Modification de la structure du Reseau (cf main)
-                        Variables obsolËtes :
-                            m_entrees   (10/01/17)
-                            m_sorties   (10/01/17)
-                        Fonctions obsolËtes :
-                        Variables ‡ modifier :
-                            m_liaisons
-                                -> tableau (taille fixe, dim 3) de Liaisons (pas pointeurs)
-                        Fonctions ‡ modifier :
-                            Constructeurs
-                            Destructeur
-                            Accesseurs (get+set)
-                            calculeSortie   (11/01/17)
-                        Variables ‡ ajouter :
-                            m_neurones  (10/01/17)
-                                -> tableau (taille fixe, dim 2) de Neurones (pas pointeurs)
-                            Taille des diffÈrentes listes
-                        Fonctions ‡ ajouter :
-                            calculNeurone   (10/01/17)
+        Cr√©√© : 09/09/16
+        D√©laration de la classe r√©seau
 */
 
 #ifndef DEF_RESEAU
@@ -48,7 +20,7 @@ class Reseau
     public :
 
     Reseau(); //1
-    Reseau(std::vector<unsigned int> nbNeuroneCouche, int mode);   //2 mode -> 0 : Initialisation neutre, 1 : Initialisation alÈatoire
+    Reseau(std::vector<unsigned int> nbNeuroneCouche, int mode);   //2 mode -> 0 : Initialisation neutre, 1 : Initialisation al√©atoire
     ~Reseau();
 
     Neurone getEntree(unsigned int i);
@@ -56,7 +28,7 @@ class Reseau
     Liaison getLiaison(unsigned int i, unsigned int j, unsigned int k);
     int getNbEntrees();
     int getNbSorties();
-    bool setEntrees(std::vector<bool> entrees);     // Retourne 1 si les entrees ont ÈtÈ initialisÈes
+    bool setEntrees(std::vector<bool> entrees);     // Retourne 1 si les entrees ont √©t√© initialis√©es
     void setEntreesActuelles(bool actuelles);
 
     std::vector<bool> calculeSorties();
